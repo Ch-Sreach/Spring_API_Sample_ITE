77c70e3f93bf221c7f1e4f0f6facdfa51a817e2b
Ch-Sreach <chsovareach07@gmail.com>
2026-06-23 17:05:49 +0700

    update without .env

--- src/main/java/com/example/productsampleapi/entity/Category.java
+++ src/main/java/com/example/productsampleapi/entity/Category.java
@@ -3,6 +3,7 @@ package com.example.productsampleapi.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @AllArgsConstructor
@@ -11,13 +12,23 @@ import java.util.List;
 @Setter
 @ToString
 @Entity(name = "category_tbl")
+@Table(name = "category_tbl")
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String description;
-    private Boolean isDeleted;
+    private Boolean isDeleted = false;
+    private String icon;    // store the icon url
+
+//    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_category_id")
+    private Category parentCategory;
+
+    @OneToMany(mappedBy = "parentCategory")
+    private List<Category> subCategory = new ArrayList<>();
 
     // one category can have many products
     @OneToMany(mappedBy = "category")
